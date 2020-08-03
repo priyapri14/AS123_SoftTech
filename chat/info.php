608c109cbd0ded307c23997bcd3fa207b319e53f
priyapri14 <63345683+priyapri14@users.noreply.github.com>
2020-08-03 15:46:07 +0530

    Social media clone

---
+++ chat/info.php
@@ -0,0 +1,13 @@
+<?php
+// Software Name
+$name = 'phpSocial';
+
+// Software Author
+$author = 'phpSocial';
+
+// Software URL
+$url = 'http://phpsocial.com';
+
+// Software Version
+$version = '3.0.8';
+?>
