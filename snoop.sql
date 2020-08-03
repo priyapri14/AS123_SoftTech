ee94dc858436dd59f9ac9f6ccd6c4aab6253a001
priyapri14 <63345683+priyapri14@users.noreply.github.com>
2020-08-03 15:34:07 +0530

    sql

--- snoop.sql
+++ snoop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 01, 2020 at 07:19 PM
+-- Generation Time: Aug 03, 2020 at 11:54 AM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.6
 
@@ -79,11 +79,12 @@ CREATE TABLE `report1` (
 --
 
 INSERT INTO `report1` (`sno`, `uname`, `email`, `vkey`, `verif`, `time1`) VALUES
-(27, 'roshan', 'roshanpenielis@gmail.com', 6509, 1, '2020-06-13 00:03:16'),
+(27, 'rp', 'roshanpenielis@gmail.com', 5435, 1, '2020-06-13 00:03:16'),
 (28, 'sun', 'upleasebe@gmail.com', 4879, 1, '2020-06-13 00:12:44'),
-(29, 'a', 'a@gmail.com', NULL, 0, '2020-07-24 19:01:09'),
-(30, 'rp', 'rp@gmail.com', 4658, 1, '2020-07-30 11:37:22'),
-(31, 'sun', 'sun@gmail.com', 5762, 1, '2020-07-30 12:29:13');
+(29, '', 'a@gmail.com', 6508, 1, '2020-07-24 19:01:09'),
+(30, 'rp', 'rp@gmail.com', 9185, 1, '2020-07-30 11:37:22'),
+(31, 'sun', 'sun@gmail.com', 5762, 1, '2020-07-30 12:29:13'),
+(32, 'z', 'z@mial.com', 5361, 1, '2020-08-02 23:50:58');
 
 -- --------------------------------------------------------
 
@@ -103,25 +104,32 @@ CREATE TABLE `report2` (
   `susid` varchar(20) DEFAULT NULL,
   `complain` varchar(100) NOT NULL,
   `time` varchar(10) NOT NULL DEFAULT current_timestamp(),
-  `attempts` int(1) NOT NULL
+  `attempts` int(1) NOT NULL,
+  `warning` int(1) NOT NULL DEFAULT 0,
+  `seen` int(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `report2`
 --
 
-INSERT INTO `report2` (`sno`, `uname`, `email`, `state`, `district`, `medium`, `category`, `userid`, `susid`, `complain`, `time`, `attempts`) VALUES
-(28, 'rp', 'rp@gmail.com', 'Jammu and Kashmir', 'as', 'Social', 'Taking advantage of ', 'rp', 'rosh', 'sf', '2020-07-30', 1),
-(29, 'rp', 'rp@gmail.com', 'Jammu and Kashmir', 'as', 'Social', 'Taking advantage of ', 'rp', 'rosh', 'sf', '2020-07-30', 2),
-(30, 'rp', 'rp@gmail.com', 'Jammu and Kashmir', 'as', 'Social', 'Taking advantage of ', 'rp', 'rosh', 'sf', '2020-07-30', 3),
-(31, 'rp', 'rp@gmail.com', 'Jammu and Kashmir', 'as', 'Social', 'Taking advantage of ', 'rp', 'rosh', 'sf', '2020-07-30', 4),
-(32, 'rp', 'rp@gmail.com', 'Jammu and Kashmir', 'as', 'Social', 'Taking advantage of ', 'rp', 'rosh', 'sf', '2020-07-30', 5),
-(33, 'sun', 'sun@gmail.com', 'Madya Pradesh', 'c', 'Social', 'Taking advantage of ', 'as', 'as', 'zx', '2020-07-30', 1),
-(34, 'sun', 'sun@gmail.com', 'Madya Pradesh', 'c', 'Social', 'Taking advantage of ', 'as', 'rith', 'zx', '2020-07-30', 2),
-(35, 'sun', 'sun@gmail.com', 'Madya Pradesh', 'c', 'Social', 'Taking advantage of ', 'as', 'rith', 'zx', '2020-07-30', 3),
-(36, 'sun', 'sun@gmail.com', 'Madya Pradesh', 'c', 'Social', 'Taking advantage of ', 'as', 'rith', 'zx', '2020-07-30', 4),
-(37, 'sun', 'sun@gmail.com', 'Madya Pradesh', 'c', 'Social', 'Taking advantage of ', 'as', 'rith', 'zx', '2020-07-30', 5),
-(38, 'roshan', 'roshanpenielis@gmail.com', 'Tamil Nadu', 'cbe', 'Social', 'Cyber Stalking', 'rp', 'rosh', 'sample', '2020-07-30', 1);
+INSERT INTO `report2` (`sno`, `uname`, `email`, `state`, `district`, `medium`, `category`, `userid`, `susid`, `complain`, `time`, `attempts`, `warning`, `seen`) VALUES
+(28, 'rp', 'rp@gmail.com', 'Jammu and Kashmir', 'as', 'Social', 'Taking advantage of ', 'rp', 'rosh', 'sf', '2020-07-30', 1, 0, 1),
+(29, 'rp', 'rp@gmail.com', 'Jammu and Kashmir', 'as', 'Social', 'Taking advantage of ', 'rp', 'rosh', 'sf', '2020-07-30', 2, 0, 1),
+(30, 'rp', 'rp@gmail.com', 'Jammu and Kashmir', 'as', 'Social', 'Taking advantage of ', 'rp', 'rosh', 'sf', '2020-07-30', 3, 0, 1),
+(31, 'rp', 'rp@gmail.com', 'Jammu and Kashmir', 'as', 'Social', 'Taking advantage of ', 'rp', 'rosh', 'sf', '2020-07-30', 4, 0, 1),
+(32, 'rp', 'rp@gmail.com', 'Jammu and Kashmir', 'as', 'Social', 'Taking advantage of ', 'rp', 'rosh', 'sf', '2020-07-30', 5, 0, 1),
+(33, 'sun', 'sun@gmail.com', 'Madya Pradesh', 'c', 'Social', 'Taking advantage of ', 'as', 'as', 'zx', '2020-07-30', 1, 0, 1),
+(34, 'sun', 'sun@gmail.com', 'Madya Pradesh', 'c', 'Social', 'Taking advantage of ', 'as', 'rith', 'zx', '2020-07-30', 2, 0, 1),
+(35, 'sun', 'sun@gmail.com', 'Madya Pradesh', 'c', 'Social', 'Taking advantage of ', 'as', 'rith', 'zx', '2020-07-30', 3, 0, 1),
+(36, 'sun', 'sun@gmail.com', 'Madya Pradesh', 'c', 'Social', 'Taking advantage of ', 'as', 'rith', 'zx', '2020-07-30', 4, 0, 1),
+(37, 'sun', 'sun@gmail.com', 'Madya Pradesh', 'c', 'Social', 'Taking advantage of ', 'as', 'rith', 'zx', '2020-07-30', 5, 0, 1),
+(41, 'z', 'z@mial.com', 'Andra Pradesh', 'as', 'Social', 'Cyber Bullying', 'as', 'rith', 'ds', '2020-08-02', 1, 0, 1),
+(42, 'z', 'z@mial.com', 'Andra Pradesh', 'as', 'Social', 'Cyber Bullying', '', 'rosh', 'ds', '2020-08-02', 2, 0, 1),
+(44, 'z', 'z@mial.com', 'Andra Pradesh', 'as', 'Social', 'Cyber Bullying', '', 'rosh', 'ds', '2020-08-02', 3, 0, 1),
+(57, 'z', 'z@mial.com', 'Bihar', 'c', 'Social', 'Assaulting Verbally', 'rp', 'rosh', 'tfd', '2020-08-03', 4, 1, 1),
+(58, 'rp', 'roshanpenielis@gmail.com', 'Tamil Nadu', 'coimbatore', 'Social', 'Abusive Comments', 'rp', 'ram', 'complaint\r\n', '2020-08-03', 4, 1, 1),
+(59, 'rp', 'roshanpenielis@gmail.com', 'Haryana', 'cbe', 'Social', 'Sending Unrelated co', 'rp', 'ram', 'complaint\r\n', '2020-08-03', 5, 1, 1);
 
 --
 -- Indexes for dumped tables
@@ -171,13 +179,13 @@ ALTER TABLE `feedbackex`
 -- AUTO_INCREMENT for table `report1`
 --
 ALTER TABLE `report1`
-  MODIFY `sno` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+  MODIFY `sno` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
 
 --
 -- AUTO_INCREMENT for table `report2`
 --
 ALTER TABLE `report2`
-  MODIFY `sno` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+  MODIFY `sno` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=60;
 
 DELIMITER $$
 --
